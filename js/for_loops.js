e8080f98b8d8e70d91c9438b51a6fce38da09857
Benny Alvarez <benjamin.c.alvarez.21@gmail.com>
2021-03-26 23:19:24 -0500

    finished for loops exercise

--- js/for_loops.js
+++ js/for_loops.js
@@ -1,3 +1,21 @@
 "use strict";
 
+//function showMultiplicationTable(num) {
+ //   for (var  i = 0; i <= 10; i++);
+//        return (num * i)
 
+//}
+
+//console.log(showMultiplicationTable(3));
+
+//const number = parseInt(prompt('Enter an integer: '));
+
+//creating a multiplication table
+for(let i = 1; i <= 10; i++) {
+
+    // multiply i with number
+    const result = i * 7;
+
+    // display the result
+    console.log(`${7} * ${i} = ${result}`);
+}
