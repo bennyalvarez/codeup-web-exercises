ee97428a7414edc9b3ffda5624e4ec721c7ae798
Benny Alvarez <benjamin.c.alvarez.21@gmail.com>
2021-04-13 08:36:33 -0500

    practicing loops exercise first half

--- js/for_loops2.js
+++ js/for_loops2.js
@@ -1,15 +1,26 @@
 "use strict";
 
-//function showMultiplicationTable(num) {
-//    for (var i = 0; i > 10; i++) {
-//        return num * i;
-//    }
-//    console.log(showMultiplicationTable(7));
-//}
-
 function showMultiplicationTable(number) {
-    for(var multiplier = 1; multiplier <= 10; multiplier++) {
-        console.log(number + " * " + multiplier + " = " + number * multiplier);
+    for(var i = 1; i <= 10; i++) {
+        console.log(number + "*" + i + "=" + number * i);
     }
 }
 showMultiplicationTable(7);
+
+
+for(var i = 0; i < 10; i++) {
+    var random = Math.floor(Math.random() * 181) + 20;
+    if( random % 2 === 0) {
+        console.log(random + " is even!");
+    } else {
+        console.log(random + " is odd.")
+    }
+}
+
+for(var i=1; i<=9; i++) {
+    var duplicate = "";
+    for (var o= 0; o < i; o++) {
+        duplicate += i;
+    }
+    console.log(duplicate);
+}
