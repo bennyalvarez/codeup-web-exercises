a29597fd92e5d6df2e26e396a930de80c8b9a9c0
Benny Alvarez <benjamin.c.alvarez.21@gmail.com>
2021-04-14 23:47:56 -0500

    practiced more loops exercise and started working on flexbox-nav exercise.

--- js/for_loops2.js
+++ js/for_loops2.js
@@ -1,43 +1,49 @@
 "use strict";
 
 function showMultiplicationTable(number) {
-    for(var i = 0; i<=10; i++) {
-        console.log(number + " * " + i + " = " + number*i);
+    for(var multiplier = 1; multiplier <=10; multiplier++) {
+        console.log(number + " * " + multiplier + " = " + multiplier*number);
     }
 }
-console.log(showMultiplicationTable(8));
 console.log(showMultiplicationTable(7));
 
 
 
-for(var i = 0; i < 10; i++) {
+for(var i = 0; i<10; i++) {
     var random = Math.floor(Math.random() * 181) + 20;
-    if ( random % 2 === 0) {
+    if (random % 2 === 0) {
         console.log(random + " is even.");
     } else {
         console.log(random + " is odd.")
     }
 }
 
-for(var i = 1; i <= 9; i++) {
+for(var i = 0; i < 10; i++) {
     var duplicate = "";
-    for (var o = 0; o < i; o++) {
+    for(var o = 0; o < i; o++) {
         duplicate += i;
     }
     console.log(duplicate);
 }
 
-for (var i = 100; i > 0; i=i-5) {
+for(var i = 100; i > 0; i=i-5) {
     console.log(i);
 }
 
-var userNumber = prompt("Please provide an odd number between 1 and 50.");
-    if (userNumber % 2 === 0) {
-        alert("That is not an odd number.");
-        break;
-    } else {
 
-    }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
