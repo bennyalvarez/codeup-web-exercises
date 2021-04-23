21457e780983e5c4f35cdb7dab81bb35d18d1399
Benny Alvarez <benjamin.c.alvarez.21@gmail.com>
2021-04-23 08:26:02 -0500

    started DOM exercise

---
+++ js/dc-marvel.js
@@ -0,0 +1,10 @@
+//var accessTheDOM = document.getElementById("dc");
+//var showMe = accessTheDOM.innerHTML;
+//alert(showMe);
+
+var like = document.getElementById("like");
+var dontLike = document.getElementById("dontLike");
+var preference = "dc";
+var output = dontLike.innerHTML + ' ' + document.getElementById(preference).innerHTML;
+alert(output);
+
