03971c069d3168cdcf0b0e077fd910f4a4aa1147
Benny Alvarez <benjamin.c.alvarez.21@gmail.com>
2021-05-20 21:15:26 -0500

    testing audio files on html

--- js/promises.js
+++ js/promises.js
@@ -72,3 +72,5 @@ wait(1000).then(() => console.log("You'll see this after 1 second"));
 wait(3000).then(() => console.log("You'll see this after 3 seconds"));
 wait(5000).then(() => console.log("You'll see this after 5 seconds"));
 
+
+
